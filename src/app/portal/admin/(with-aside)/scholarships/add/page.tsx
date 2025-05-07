9b6f255b02cd9f151576984a9815182cce0cd89a
fadliarz <fadliarz03@gmail.com>
2025-05-07 10:35:39 +0700

    fix: add force dynamic

--- src/app/portal/admin/(with-aside)/scholarships/add/page.tsx
+++ src/app/portal/admin/(with-aside)/scholarships/add/page.tsx
@@ -2,16 +2,18 @@ import AdminHeader from "@/components/admin/header";
 import AdminBreadcrumb from "@/components/admin/breadcrumb";
 import Wrapper from "@/app/portal/admin/wrapper";
 import AddForm from "./add-form";
-import { getTags } from "@/_actions/tag-action";
+import {getTags} from "@/_actions/tag-action";
+
+export const dynamic = 'force-dynamic';
 
 export default async function Add() {
 
-  const tagsAll = await getTags();
-  return <>
-    <AdminHeader title='Add Scholarship'/>
-    <AdminBreadcrumb/>
-    <Wrapper>
-      <AddForm tags={tagsAll}/>
-    </Wrapper>
-  </>
+    const tagsAll = await getTags();
+    return <>
+        <AdminHeader title='Add Scholarship'/>
+        <AdminBreadcrumb/>
+        <Wrapper>
+            <AddForm tags={tagsAll}/>
+        </Wrapper>
+    </>
 } 
