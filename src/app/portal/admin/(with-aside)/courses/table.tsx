a0040d2cc8d224532e6fedb397b25eefee19a331
jhoniananta <jhoniananta@gmail.com>
2025-05-07 21:44:16 +0700

    feat: add attachment and fix courses table

--- src/app/portal/admin/(with-aside)/courses/table.tsx
+++ src/app/portal/admin/(with-aside)/courses/table.tsx
@@ -37,7 +37,7 @@ function CoursesTable({data}: { data: $CourseAPI.GetCourses.Response["data"] }) 
             <Table className='table-admin'>
                 <TableHeader>
                     <TableRow>
-                        <TableHead>Title</TableHead>
+                        <TableHead className='w-64 min-w-[16rem]'>Title</TableHead>
                         <TableHead>Description</TableHead>
                         <TableHead>Students</TableHead>
                         <TableHead>Instructors</TableHead>
@@ -62,7 +62,7 @@ function CoursesTable({data}: { data: $CourseAPI.GetCourses.Response["data"] }) 
 
                         return (
                             <TableRow key={course.courseId} className='even:bg-navy/5 odd:bg-transparent'>
-                                <TableCell>{course.title}</TableCell>
+                                <TableCell className='w-64 min-w-[16rem] whitespace-nowrap text-nowrap'>{course.title}</TableCell>
                                 <TableCell
                                     className='whitespace-nowrap text-nowrap'>{description.length > 100 ? description.slice(0, 100) + "..." : description}</TableCell>
                                 <TableCell>{course.numberOfStudents}</TableCell>
