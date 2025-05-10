fdcefd97418a58ae6acf8d50d2e0d45730e581eb
fadliarz <fadliarz03@gmail.com>
2025-05-10 11:10:12 +0700

    chore: update flex

--- src/app/portal/admin/(with-aside)/courses/add/page.tsx
+++ src/app/portal/admin/(with-aside)/courses/add/page.tsx
@@ -1,15 +1,19 @@
 import AdminHeader from "@/components/admin/header";
 import AddForm from "./add-form";
+import Wrapper from "../../../wrapper";
+import AdminBreadcrumb from "@/components/admin/breadcrumb";
 
 export default async function Add() {
 
     return (
         <>
             <AdminHeader title='Add Course'/>
-            {/*<AdminBreadcrumb/>*/}
-            {/*<Wrapper>*/}
-            <AddForm/>
-            {/*</Wrapper>*/}
+            <AdminBreadcrumb/>
+            <div className={'flex-2'}>
+                <Wrapper>
+                    <AddForm/>
+                </Wrapper>
+            </div>
         </>
     );
 }
